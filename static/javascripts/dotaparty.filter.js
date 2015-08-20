d7100eaa1586b5125112a08299756cbe3db90024
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-08-20 18:24:24 -0300

    abbreviation filter for big numbers color for k/d/a

--- static/javascripts/dotaparty.filter.js
+++ static/javascripts/dotaparty.filter.js
@@ -1,11 +1,23 @@
 (function () {
     'use strict';
 
-    angular
-        .module('dotaparty.filter')
-        .filter('secondsToDateTime', [function () {
+    var module = angular.module('dotaparty.filter');
+    module.filter('secondsToDateTime', [function () {
             return function (seconds) {
                 return new Date(1970, 0, 1).setSeconds(seconds);
             };
         }])
+
+    module.filter('abbreviate', [function() {
+        return function(number) {
+            if (number < 1000)
+                return number.toString();
+
+            var milhares = Math.floor(number / 1000);
+            var centenas = number % 1000;
+
+            return milhares.toString() + '.' + centenas.toString()[0] + 'k  ';
+
+        }
+    }])
 })();
