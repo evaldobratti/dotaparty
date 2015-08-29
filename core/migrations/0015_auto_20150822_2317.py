fd0e6f448cc5fe4c8b6bf7a3615aab2035dba2db
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-08-28 22:20:10 -0300

    fix migration

--- core/migrations/0015_auto_20150822_2317.py
+++ core/migrations/0015_auto_20150822_2317.py
@@ -4,9 +4,7 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 from core import parameters
 def create_parameters(apps, schema_editor):
-    Parameter = apps.get_model("core", "Parameter")
-
-    Parameter.objects.create(name=parameters.LAST_MATCH_ID_SKILL_UNDEFINED, value=None)
+    pass
 
 class Migration(migrations.Migration):
 
