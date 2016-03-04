96cf4e944b35635dd6f09f97de1e52c2a2a5150d
Evaldo Bratti <evaldo.bratti@gmail.com>
2016-03-03 22:38:59 -0300

    fixing downloader

--- core/downloader/matches.py
+++ core/downloader/matches.py
@@ -21,19 +21,19 @@ class DownloaderGamesBySeqNum(object):
             last_match_seq_num = self.__get_last_match_seq_num()
             try:
                 matches = d2api.get_matches_seq(last_match_seq_num)
-                for match in matches.matches:
+                for match in matches['matches']:
                     self.__download_match_if_interested(match)
-                    LAST_MATCH_SEQ_NUM.set_value(match.match_seq_num)
+                    LAST_MATCH_SEQ_NUM.set_value(match['match_seq_num'])
             except Exception, e:
                 print e
                 get_logger().exception(e)
 
     def __download_match_if_interested(self, match):
-        get_logger().info("analysing match_id : {} seq_num: {}".format(match.match_id, match.match_seq_num))
+        get_logger().info("analysing match_id : {} seq_num: {}".format(match['match_id'], match['match_seq_num']))
 
-        if [p for p in match.players if p.account_id in INTERESTED_ACCOUNTS_IDS.value()]:
-            get_logger().info("will download match_id : {} seq_num: {}".format(match.match_id, match.match_seq_num))
-            tasks.schedule_download_match(match.match_id, False)
+        if [p for p in match['players'] if p['account_id'] in INTERESTED_ACCOUNTS_IDS.value()]:
+            get_logger().info("will download match_id : {} seq_num: {}".format(match['match_id'], match['match_seq_num']))
+            tasks.schedule_download_match(match['match_id'], False)
 
     def __get_last_match_seq_num(self):
         last_match_seq_num = LAST_MATCH_SEQ_NUM.value()
