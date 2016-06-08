12d8d4c2475a5f2b22414b69bbc6309cf26924f9
Evaldo Bratti <evaldo.bratti@gmail.com>
2016-06-07 21:50:29 -0300

    fix

--- api/middleware.py
+++ api/middleware.py
@@ -4,14 +4,14 @@ from django.db.models import F
 
 class VisitMiddleware(object):
     def process_request(self, request):
-        visit, _ = models.Visit.objects.get_or_create(host=self.get_ip(request), requested=request.get_full_path())
+        visit, _ = models.Visit.objects.get_or_create(host=self.get_ip(request)[:400],
+                                                      requested=request.get_full_path()[:400])
         visit.count = F('count') + 1
         visit.save()
 
     def get_ip(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
+            return x_forwarded_for.split(',')[0]
         else:
-            ip = request.META.get('REMOTE_ADDR')
-        return ip
+            return request.META.get('REMOTE_ADDR')
