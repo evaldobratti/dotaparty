193037b493db35eaa92df45af69b1a2bf0ec4cf6
Evaldo Bratti <evaldo.bratti@gmail.com>
2016-09-17 17:30:23 -0300

    reseting command

--- core/management/commands/run_downloader.py
+++ core/management/commands/run_downloader.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
+from core.parameters import LAST_MATCH_SEQ_NUM
 from core.downloader import matches
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        LAST_MATCH_SEQ_NUM.reset()
         matches.execute_download_games()
